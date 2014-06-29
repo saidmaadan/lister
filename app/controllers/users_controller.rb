aed748087dd94b097d39cd216b6460e64e1f2316
Said Maadan <saidmaadan@gmail.com>
2014-06-29 16:51:48 -0500

    redesign of index, all listings page and user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,7 @@ class UsersController < ApplicationController
 
 	def new
 		@user = User.new
+    @review = @user.reviews.new
 	end
 
 	def create
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,8 +7,8 @@ class Listing < ActiveRecord::Base
 
 
 	has_attached_file :upload, styles: {
-    :small => "200x200>", :medium => "350x300>",
-    :large => "500x500>", :thumb => "100x100>"
+    :small => "200x200>", :medium => "300x300>",
+    :large => "600x500>", :thumb => "100x100>"
   }, :default_url => "/:style/mks7.jpg"
 
   validates_attachment_content_type :upload, :content_type => /\Aimage\/.*\Z/
@@ -23,9 +23,15 @@ class Listing < ActiveRecord::Base
 #     {:bucket => "saidmfola", :access_key_id => "AKIAIHKZTDWOW7I433ZQ", :secret_access_key => "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"}
 #   end
 
-  validates :title, :address, :bedroom, :bathroom, :apartment_type, :contact_name, :phone, presence: true
+  validates :title, :address, :bedroom, :bathroom, :contact_name, :phone, presence: true
   validates :summary, length: { minimum: 25 }
   validates :pricing, numericality: { greater_than_or_equal_to: 0 }
+
+  # APARTMENT_TYPE = ['Apartment', 'home', 'Shared Room', 'Private room', 'Other']
+  # validates :apartment_type, inclusion: { in: APARTMENT_TYPE }
+
+  # USER_TYPE = ['MANAGEMENT COMPANY', 'OWNER', 'AGENCY', 'BROKER', 'TENANT']
+  # validates :user_type, inclusion: { in: USER_TYPE } 
 
   has_many :enquiries, dependent: :destroy
 
